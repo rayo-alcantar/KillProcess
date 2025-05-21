b46ede1be16c610768266871798d3a5211be9dd3
Ángel Alcántar <rayoalcantar@gmail.com>
2025-05-21 11:15:47 -0600

    se prepara release para 2025.1

---
+++ .py
@@ -0,0 +1,12 @@
+* [33me8ff2a0[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;33mtag: [m[1;33m2.1.2[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m prueba
+* [33mf565193[m Se aregla installtask
+* [33mc21d3e5[m[33m ([m[1;33mtag: [m[1;33m2.1.1[m[33m)[m Se aprepara el readme
+* [33mc5490cb[m se prepara nueva versión y se añade línea al readme
+*   [33md689894[m Merge pull request #7 from CyrilleB79/translations
+[32m|[m[33m\[m  
+[32m|[m * [33m0509556[m Update translations fr, en and it
+[32m|[m[32m/[m  
+* [33m64fe0a2[m Se corrige el nombre del autor para empezar a hacer una nomenclatura con el
+* [33m762e496[m se añade pantalla de doanción
+* [33mb8dad4b[m[33m ([m[1;33mtag: [m[1;33m2.1[m[33m)[m se hacen traducibles algunas líneas faltantes
+* [33m84dfdf8[m[33m ([m[1;33mtag: [m[1;33m2.0[m[33m)[m hago cambios con las tags
